2d5b94dbe1d79134f120e4d6b6046381b5167702
Munis Isazade <munisisazade@gmail.com>
2017-02-19 04:04:48 +0400

    fix url

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,6 @@ from news.views import TestView, index
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^index.asp$', TestView.as_view(), name='main-index')
+    url(r'^index.aspx$', TestView.as_view(), name='main-index')
     # url(r'^',include('news.urls')),
 ]
