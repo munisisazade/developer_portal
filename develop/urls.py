40709e340a8ca4213bac8df6e098fe583a60a996
Munis Isazade <munisisazade@gmail.com>
2017-02-20 14:00:06 +0400

    change

--- develop/urls.py
+++ develop/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-handler404 = 'news.views.NotFound404.as_view()'
+handler404 = 'news.views.NotFound404'
 
 urlpatterns = [
     url(r'^Adminqaqalar.aspx', admin.site.urls),
