bce4efd6625e0cbdb943feed4f1ab3adb9e88ebd
Munis Isazade <munisisazade@gmail.com>
2017-02-20 13:38:23 +0400

    debug false

--- develop/settings.py
+++ develop/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-   '/home/munis/Documents/unicopycenter/static',
-]
+STATIC_ROOT = 'static'
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#    '/home/munis/Documents/unicopycenter/static',
+# ]
 
 
 MEDIA_URL = '/media/'
