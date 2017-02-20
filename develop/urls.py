fbefeb72035d5bf06dfd04a1c309a6292116d8d9
Munis Isazade <munisisazade@gmail.com>
2017-02-20 14:20:14 +0400

    customize errors

--- develop/urls.py
+++ develop/urls.py
@@ -13,17 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include, handler404,handler403,handler404,handler500
+from django.conf.urls import url, include, handler404,handler403,handler404,han
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from news.views import NotFound404
-
-handler400 = NotFound404
-handler403 = NotFound404
-handler404 = NotFound404
-handler500 = NotFound404
+# 
+# handler400 = NotFound404
+# handler403 = NotFound404
+# handler404 = NotFound404
+# handler500 = NotFound404
 
 urlpatterns = [
     url(r'^Adminqaqalar.aspx', admin.site.urls),
