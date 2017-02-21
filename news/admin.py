d90b6d934d9f334e0ee6d26d18677ae0ded00892
Munis Isazade <munisisazade@gmail.com>
2017-02-22 03:20:29 +0400

    Create Database

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from news.models import Slider,How_it_works
+from news.models import Slider,How_it_works,Author,ArticleCategory,ArticleTags,RelationTagArticle,RelationCategoryArticle,ArticleImages,Article,Mesagges,Contact_us,PrivacyPolicy
 # Register your models here.
 
 
@@ -12,3 +12,15 @@ class SliderAdmin(admin.ModelAdmin):
 
 admin.site.register(Slider,SliderAdmin)
 admin.site.register(How_it_works)
+admin.site.register(Author)
+admin.site.register(ArticleCategory)
+admin.site.register(ArticleTags)
+admin.site.register(RelationTagArticle)
+admin.site.register(RelationCategoryArticle)
+admin.site.register(ArticleImages)
+admin.site.register(Article)
+admin.site.register(Mesagges)
+admin.site.register(Contact_us)
+admin.site.register(PrivacyPolicy)
+
+
