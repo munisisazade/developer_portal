ef0a1269372b3499bdee941a1489d5f0dcbe1e83
Munis Isazade <munisisazade@gmail.com>
2017-02-21 18:35:16 +0400

    Add About html

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from news.views import TestView, index
+from news.views import TestView, index,AboutView
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'^index.aspx$', TestView.as_view(), name='main-index')
+    url(r'^index.aspx$', TestView.as_view(), name='main-index'),
+    url(r'^about-us.aspx$', AboutView.as_view(), name='about'),
     # url(r'^',include('news.urls')),
 ]
