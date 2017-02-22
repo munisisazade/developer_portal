bdf5a9a3bfe09fa876b5f46947b9211f66f5273e
Munis Isazade <munisisazade@gmail.com>
2017-02-22 17:08:41 +0400

    fix error

--- develop/middleware/add_fake_middleware.py
+++ develop/middleware/add_fake_middleware.py
@@ -1,13 +1,13 @@
-class MyCookieProcessingMiddleware(object):
-
-    # your desired cookie will be available in every django view
-    def process_request(self, request):
-        # will only add cookie if request does not have it already
-        if not request.META.get('X-Powered-By '):
-            request.META['X-Powered-By '] = 'ASP.NET'
-
-    # your desired cookie will be available in every HttpResponse parser like browser but not in django view
-    def process_response(self, request, response):
-        if not request.META.get('X-Powered-By'):
-            response.META['X-Powered-By '] = 'ASP.NET'
-        return response
+# class MyCookieProcessingMiddleware(object):
+#
+#     # your desired cookie will be available in every django view
+#     def process_request(self, request):
+#         # will only add cookie if request does not have it already
+#         if not request.META.get('X-Powered-By '):
+#             request.META['X-Powered-By '] = 'ASP.NET'
+#
+#     # your desired cookie will be available in every HttpResponse parser like browser but not in django view
+#     def process_response(self, request, response):
+#         if not request.META.get('X-Powered-By'):
+#             response.META['X-Powered-By '] = 'ASP.NET'
+#         return response
