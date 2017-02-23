bbc13383ae9e6b69d8cbbd47c41957cfea8998d7
Munis Isazade <munisisazade@gmail.com>
2017-02-23 16:06:30 +0400

    add signal file

---
+++ news/signals.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from news.models import Slider,ArticleCategory
+import os
+
+
+@receiver(post_delete, sender=Slider, dispatch_uid="delete_slider")
+def delete_slider(sender,**kwargs):
+    image = kwargs.get('instance')
+    file = os.path.join(settings.BASE_DIR,image)
+    if os.path.isfile(file):
+        os.remove(file)
+        obj = ArticleCategory(title='Obyekt silindi')
+        obj.save()
+    else:
+        obj = ArticleCategory(title='Obyekt silinmedi')
+        obj.save()
+
+
