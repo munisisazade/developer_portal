bc92fad6a3eae4a7a4e6f4a84aeb96b7c28e7855
Munis Isazade <munisisazade@gmail.com>
2017-02-25 17:12:09 +0400

    add coming soon

--- news/urls.py
+++ news/urls.py
@@ -3,7 +3,7 @@ from news.views import TestView, index,AboutView,GalleryView,ContactsView,Privac
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    # url(r'^index.aspx$', TestView.as_view(), name='main-index'),
+    url(r'^index.aspx$', index, name='main-index'),
     # url(r'^about-us.aspx$', AboutView.as_view(), name='about'),
     # url(r'^gallery.aspx$', GalleryView.as_view(), name='gallery'),
     # url(r'^contact.aspx$', ContactsView.as_view(), name='contact'),
