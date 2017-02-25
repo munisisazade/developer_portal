cbbd41e70919b96a346e7a2cf0d28c6b1b206cd6
Munis Isazade <munisisazade@gmail.com>
2017-02-25 20:30:52 +0400

    Update url on Rest api

--- develop/settings.py
+++ develop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ermn@t3e6)2lwtgca9nfyxf$h6b9fpo%(!h%mtgt7tyy2ut6m*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 if not DEBUG:
