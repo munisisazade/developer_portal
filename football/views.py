cba9f5636133ea922b6eff996db12714385e564a
Munis Isazade <munisisazade@gmail.com>
2017-04-03 20:25:46 +0400

    Best scrapper

--- football/views.py
+++ football/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+"""
+    Scraping with JS support:
+
+>>> import dryscrape
+>>> from bs4 import BeautifulSoup
+>>> session = dryscrape.Session()
+>>> session.visit(<MY_GRAB_URL>)
+>>> response = session.body()
+>>> soup = BeautifulSoup(response)
+>>> soup.find(id="intro-text")
+<p id="intro-text">Yay! Supports javascript</p>
+
+    Find by all attribute name element:
+
+    myfilter = soup.find_all("<DOM_ELEMENT_NAME>", { "<ATTRIBUTE_NAME>" : "<ATTRIBUTE_VALUE>" })
+
+
+
+"""
