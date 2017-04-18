f4c9b193bda5836888e8b90c907bcf5d695401b0
Munis Isazade <munisisazade@gmail.com>
2017-04-18 19:49:22 +0400

    update git push

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import url, include
-from rest_framework_jwt.views import obtain_jwt_token
 from api.views import UserList,BrandDetail,NewsList,ArticleDetail
 
 
@@ -9,5 +8,5 @@ urlpatterns = [
     url(r'^news-list/$', NewsList.as_view(), name='news-list'),
     url(r'^article/(?P<pk>[0-9]+)$', ArticleDetail.as_view(), name='article-detail'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_jwt_token),
+
 ]
